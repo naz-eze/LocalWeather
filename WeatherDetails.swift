3e1737254728e0fff2d8791b544208f338371081
nazcompile <nchinazaeze@gmail.com>
2015-08-30 19:19:02 +0100

    New Structure to hold weather details

---
+++ WeatherDetails.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherValue.swift
+//  Local Temperature
+//
+//  Created by Naz on 30/08/2015.
+//  Copyright (c) 2015 nazchinaza. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherDetails {
+    
+    var location: String
+    var averageTemperature: Double
+    var forecast: String
+    
+    var humidity: Double
+    var windspeed: Double
+    var windDirection: Double
+    
+    var weatherIcon: String
+    
+    var sunrise: String
+    var sunset: String
+    
+    var lastUpdated: String
+    
+    func logDetails() {
+         NSLog("[WeatherDetails] Weather values = location: \(location). averageTemperature: \(averageTemperature)K."
+                + " forecast: \(forecast). humidity: \(humidity). weatherIcon: \(weatherIcon)."
+                + " windspeed: \(windspeed). windDirection: \(windDirection). sunrise: \(sunrise)."
+                + " sunset: \(sunset). lastUpdated: \(lastUpdated)")
+    }
+}
