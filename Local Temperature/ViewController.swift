2ab73834c96cf2a52136a8062fa8ae7018c88049
nazcompile <nchinazaeze@gmail.com>
2015-08-31 16:34:12 +0100

    Modified controller class to work with redesigned UI

--- Local Temperature/ViewController.swift
+++ Local Temperature/ViewController.swift
@@ -10,59 +10,87 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+  
     
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var forecastLabel: UILabel!
-    @IBOutlet weak var humidityLabel: UILabel!
-    @IBOutlet weak var windSpeedLabel: UILabel!
     @IBOutlet weak var sunriseLabel: UILabel!
     @IBOutlet weak var sunsetLabel: UILabel!
-    @IBOutlet weak var buttonRef: UIButton!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    @IBOutlet weak var forecastIcon: UIImageView!
+
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    @IBOutlet weak var tempSegmentControl: UISegmentedControl!
     
     lazy var userLocation = UserLocation()
     lazy var weatherService = WeatherService()
-
+    let colours = Colours()
+    var weatherDetails: WeatherDetails?
     
+    
+    @IBAction func tempSegmentChange(sender: UISegmentedControl) {
+        if let weatherDet = weatherDetails {
+            switch tempSegmentControl.selectedSegmentIndex {
+            case 0:
+                self.temperatureLabel.text = String(format: "%.0f", weatherDet.averageTemperature.celcius)
+            case 1:
+                self.temperatureLabel.text = String(format: "%.0f", weatherDet.averageTemperature.fahrenheit)
+            case 2:
+                self.temperatureLabel.text = String(format: "%.0f", weatherDet.averageTemperature.kelvin)
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction func reload(sender: UIButton) {
+        self.loading.startAnimating()
+        userLocation.parent = self
+        userLocation.getLocationDetails()
+    }
+    
+    func refresh() {
+        view.backgroundColor = UIColor.clearColor()
+        var backgroundLayer = colours.gl
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, atIndex: 0)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setButtonProperties()
-        // Do any additional setup after loading the view, typically from a nib.
+        refresh()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func runApp(sender: UIButton) {
-        userLocation.parent = self
-        userLocation.getLocationDetails()
-    }
-    
+
     func foundUserLocation(longitude: String, latitude: String) -> Void { //TODO: Refactor to event listener
         weatherService.parent = self
         weatherService.getWeatherDetails(longitude, userLatitude: latitude)
     }
     
     func didGetWeatherDetails(weatherDetails: WeatherDetails) -> Void { //TODO: Refactor to event listener
+        self.weatherDetails = weatherDetails
+        self.setDisplayValues(weatherDetails)
+    }
+    
+    func setDisplayValues(weatherDetails: WeatherDetails) -> Void {
         dispatch_async(dispatch_get_main_queue()) {
+            self.loading.stopAnimating()
             self.locationLabel.text = weatherDetails.location
-            self.temperatureLabel.text = String(stringInterpolationSegment: weatherDetails.averageTemperature)
+            self.temperatureLabel.text = String(format: "%.0f", weatherDetails.averageTemperature.celcius)
             self.forecastLabel.text = weatherDetails.forecast
-            self.humidityLabel.text = String(stringInterpolationSegment: weatherDetails.humidity) + "%"
-            self.windSpeedLabel.text = String(stringInterpolationSegment: weatherDetails.windspeed) + "meter/sec"
+            self.humidityLabel.text = String(format: "%.1f", weatherDetails.humidity)
+            self.windSpeedLabel.text = String(format: "%.1f", weatherDetails.windspeed)
             self.sunriseLabel.text = weatherDetails.sunrise
             self.sunsetLabel.text = weatherDetails.sunset
         }
     }
-    
-    private func setButtonProperties() -> Void {
-        buttonRef.layer.cornerRadius = 10;
-        buttonRef.layer.borderWidth = 0.75;
-        buttonRef.backgroundColor = UIColor.clearColor()
-        buttonRef.layer.borderColor = UIColor.lightGrayColor().CGColor
-    }
 
 }
-
